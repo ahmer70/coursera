9da6822efb3c24e1ddcf112529fc459ddc725d40
Ahmer Arain <57433886+ahmerarain@users.noreply.github.com>
2021-12-27 00:47:31 +0800

    Menu Components part1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import MenuComponent from './components/MenuComponent';
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
           <NavbarBrand href='/'>ConFusion</NavbarBrand>
         </div>
       </Navbar>
+      <MenuComponent/>
     </>
   );
 }
